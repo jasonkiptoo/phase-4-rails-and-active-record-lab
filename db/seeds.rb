cef955d8fe7c20a3f355b02dd83209c80a170c2a
JasonKiptoo <kiptoojason@gmail.com>
2022-11-21 22:33:04 +0300

    completed assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create!([{first_name: "Jayson", last_name: "Kiptoo"},{first_name: "Jeff", last_name: "Kip"}])
